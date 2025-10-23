0a7fe5fbc4cd70984d2c712356d1157329cef0a8
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-10-23 10:38:38 +0200

    add base code for context

--- src/context/user.tsx
+++ src/context/user.tsx
@@ -1,1 +1,52 @@
 //TODO: Implement user context
+
+import { createContext, useContext, useState, type PropsWithChildren } from "react";
+
+type UserState = {
+    user: UserProfile | null;
+    actions: {
+        signin: (email: string, password: string) => Promise<void>,
+        //TODO REGISTER
+        //TODO LOGOUT
+    }
+}
+
+const defaultState: UserState = {
+    user: null,
+    actions: {
+        signin: () => Promise.resolve()
+    }
+}
+
+const UserContext = createContext(defaultState)
+
+function UserProvider ({children}: PropsWithChildren) {
+    const [user, setUser] = useState(defaultState.user)
+
+    //TODO: get user through auth service on load
+
+    const signin: typeof defaultState.actions.signin = async (email, password) => {
+        //TODO Move signin code here
+        //TODO: get user through auth service 
+    }
+
+    return (
+        <UserContext.Provider value={{
+            user,
+            actions: {
+                signin
+            }
+        }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+function useUser() {
+    return useContext(UserContext)
+}
+
+export {
+    UserProvider,
+    useUser
+}
